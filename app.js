0f41a3f4561a31ac569ceb25da8f2b952bdc70f1
William <williameliason8@gmail.com>
2021-01-06 16:54:03 -0600

    instantiated a player class to create player and hold their postitions on board

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 
+// ********** EVENT LISTENERS ********** //
 
+//onSubmit forms for userNames - create a player instance and also initialize the starting user by adding a class name to grid-container class list
 
 
 
@@ -10,7 +12,7 @@ for (let element of gridItems) {
   element.addEventListener("click", ()=>{
     let classList = element.classList
     if (classList.contains('clicked')) {
-      alert('piece already here');
+      alert('piece already in this position, pick an open position');
       return;
     }
     //element.innerHTML= 'x';
@@ -28,4 +30,41 @@ let clickedItems = [];
 let processClassName = (className) => {
   let length = className.length
   return className.slice(length-2);
+}
+
+/*
+storing coordinates <--- this is a naive solution .. mvp
+ •store an array of [x,y] <--- not necessary
+ •store an x obj and y obj
+ •when a new coord is stored check for a 'solution' i.e. three in a row
+    - solution can be defined as:
+      •three x's in the same row i.e. (0,0 ; 1,0 ; 2,0)
+      •three y's in the same column i.e (0,0; 0,1; 0,2)
+      •diagonal solution ()
+ */
+let yCoords = {
+  0: null,
+  1: null,
+  2: null
+}
+
+let xCoords = {
+  0: null,
+  1: null,
+  2: null
+}
+
+let diagonals = {
+  major: null,
+  minor: null
+}
+
+//a Player class for each player
+class Player {
+  constructor(name) {
+    this.name = name;
+    this.xCoords = xCoords;
+    this.yCoords = yCoords;
+    this.diagonals = diagonals;
+  }
 }
