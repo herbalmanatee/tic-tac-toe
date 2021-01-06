d73534d853a1a4500a2d3f023db31b881302bdc3
William <williameliason8@gmail.com>
2021-01-06 15:23:38 -0600

    manipulate grid item on click

--- app.js
+++ app.js
@@ -1,5 +1,31 @@
-//add a click event listener to each grid-item
+
+
+
+
+
+//add click event listener to each griditem
 let gridItems = document.querySelectorAll('.grid-item');
+
 for (let element of gridItems) {
-  element.addEventListener("click", ()=>{element.innerHTML=('x')});
+  element.addEventListener("click", ()=>{
+    let classList = element.classList
+    if (classList.contains('clicked')) {
+      alert('piece already here');
+      return;
+    }
+    //element.innerHTML= 'x';
+    element.style.backgroundColor = 'lightblue';
+    element.classList.add('clicked');
+    clickedItems.push(processClassName(element.classList[0]));
+    console.log(clickedItems);
+  });
+}
+
+
+//function to process class name into coords
+let clickedItems = [];
+
+let processClassName = (className) => {
+  let length = className.length
+  return className.slice(length-2);
 }
